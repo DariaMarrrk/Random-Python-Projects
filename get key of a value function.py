cc30c678aed6776f9a0e9026d43b7cf901c7ac76
DariaMarrrk <167748461+DariaMarrrk@users.noreply.github.com>
2024-11-14 09:54:51 +0400

    Add files via upload

---
+++ get key of a value function.py
@@ -0,0 +1,9 @@
+def get_key(dictionary, value): 
+    Keys = []
+    for key in dictionary:
+        if dictionary[thing] == value:
+            Chosen.append(key)
+    return(Keys)
+
+""" function gets the key with a specific value from a dictionary.
+In case there are multiple keys with the value, the return is a list to store them all
