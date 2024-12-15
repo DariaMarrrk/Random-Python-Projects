1ee689961b56fada8e7964bdf8a72ff1c9a7e889
DariaMarrrk <167748461+DariaMarrrk@users.noreply.github.com>
2024-12-15 14:51:29 +0400

    Update operations with intermediate results.py

--- operations with intermediate results.py
+++ operations with intermediate results.py
@@ -1,23 +1,26 @@
 ans = int(input("expected output: "))
 inputs = []
+paths = {} #key = the result, value = array of two inputs that produced this result
+global original_inputs
+original_inputs = []
 
 i = int(input("enter the inputs: "))
 while i != 0:
     if i not in inputs:
         inputs.append(i)
+        original_inputs.append(i)
     i = int(input())
 
 def operation(x, y):
     return(x+y)
 
 Found = False
-adding_to = inputs[len(inputs)-1] 
+adding_to = inputs[len(inputs)-1] #the number currently adding to
 while not Found:
     i = 0
-    print(inputs)
     while i < inputs.index(adding_to):
-        print(adding_to, inputs[i])
         output = operation(adding_to, inputs[i])
+        paths.update({output: [adding_to, inputs[i]]})
         if output == ans:
             Found = True
             break
@@ -27,7 +30,18 @@ while not Found:
         i += 1
     adding_to = inputs[inputs.index(adding_to)+1]
 
+path_to_ans = {}
 
-"""Exceptions with basic arithmetic operations:
-addition: if x+y > ans; if x and y are both even and ans is odd
-multiplication: if ans is a prime number"""
+def trace(paths, ans, path_to_ans):
+    looking_into = paths.get(ans)
+    path_to_ans.update({ans: looking_into})
+    if looking_into[0] in original_inputs and looking_into[1] in original_inputs:
+        return(str(path_to_ans))
+    else:
+        if looking_into[0] not in original_inputs:
+            trace(paths, looking_into[0], path_to_ans)
+        if looking_into[1] not in original_inputs:
+            trace(paths, looking_into[1], path_to_ans)
+            
+trace(paths, ans, path_to_ans)      
+print(path_to_ans)
